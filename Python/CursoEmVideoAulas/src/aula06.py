4f17ef7de9008535353ae9128decfc838fe4264b
martinsSam <46246000+martinsSam@users.noreply.github.com>
2019-05-01 16:13:12 -0300

    string format

--- Python/CursoEmVideoAulas/src/aula06.py
+++ Python/CursoEmVideoAulas/src/aula06.py
@@ -1,4 +1,7 @@
+#SFM 05/2019
+# A versão 3.7 do Python utiliza uma nova forma de formatação
 n1 = int(input('Digite o primeiro valor:'))
 n2 = int(input('Digite o segundo valor:'))
 s = n1+n2
-print(f'A soma de {n1} e {n2} é {s}')
+print('Versão 3.6 - A soma entre {0} e {1} é: {2}' .format(n1, n2, s))
+print(f'Versão 3.7 - A soma entre {n1} e {n2} é: {s}')
