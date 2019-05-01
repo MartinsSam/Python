4e17ba773c0fdad6f985af95a13bed36e382d0d0
martinsSam <46246000+martinsSam@users.noreply.github.com>
2019-05-01 15:05:29 -0300

    Create CursoEmVideoFolder
    Just code files will be put in gitRepository

---
+++ Python/Scripts/primeiroScript.py
@@ -0,0 +1,6 @@
+#SFM - 04.2019
+#Primeiro programa script em python. Variáveis são objetos em Python
+nome = input('Qual é o seu nome?');
+idade = input('Qual é a sua idade?');
+peso = input('Qual é o seu peso?');
+print(nome, idade, peso);
