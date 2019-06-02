90c465ffc491e7b2a77161cadb5651d484f4e75a
SFM <MartinsSam@users.noreply.github.com>
2019-06-01 23:21:12 -0300

    07CursoEmVideo class desafio

--- CursoEmVideoAulas/src_aulas/aula07a.py
+++ CursoEmVideoAulas/src_aulas/aula07a.py
@@ -1,3 +1,19 @@
 nome = input('Qual é seu nome?')
-print('Prazer em te conhecer {}!' .format(nome))
-print('Prazer em te conhecer {>20}'.format(nome))
+#String Treat
+#print('Prazer em te conhecer {}!' .format(nome))
+#print('Prazer em te conhecer {:>20}!'.format(nome))
+#print('Prazer em te conhecer {:<20}!'.format(nome))
+#print('Prazer em te conhecer {:^20}!'.format(nome))
+#print('Prazer em te conhecer {:=^20}!'.format(nome))
+
+#Number Treat
+n1 = int(input('Primeiro Valor:'))
+n2 = int(input('Segundo Valor:'))
+s = n1+n2
+m = n1*n2
+d = n1/n2
+d1 = n1//n2
+e = n1 ** n2
+print('A soma vale {}' .format(n1+n2))
+print('A soma é {}, o produto é {} e a divisão é {:.3f}'.format(s, m, d), end=' ')
+print('Divisão inteira {} e potência {}' .format(d1, e))
