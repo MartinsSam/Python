84746e183fc495627ff1ed5355ab6ee36a3e2feb
SFM <MartinsSam@users.noreply.github.com>
2019-06-22 18:56:17 -0300

    Desafio aula 07

---
+++ CursoEmVideoAulas/src_exercicios/aula07_desafio05.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um número:'))
+
+print('Número {0} , antecessor: {1} , sucessor:{2} '.format(n1, n1-1, n1+1))
---
+++ CursoEmVideoAulas/src_exercicios/aula07_desafio06.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um número:'))
+dobro = n1 *2
+triplo = n1 * 3
+rquadrada= n1**(1/2)
+
+print('Númeroo: {}, dobro: {}, triplo: {} , raiz quadrada: {:.3f}' .format(n1, dobro, triplo, rquadrada))
