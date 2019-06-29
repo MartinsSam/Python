309890d17f30eeb0f2f628d9612ac28c12df9cf2
SFM <MartinsSam@users.noreply.github.com>
2019-06-29 19:24:54 -0300

    Aula07

---
+++ CursoEmVideoAulas/src_exercicios/aula07_desafio08.py
@@ -0,0 +1,2 @@
+numero = float(input('Digite um valor(em metros):'))
+print('O valor {}, convertido para centimetros é {:.0f} ' .format(numero, numero*100))
---
+++ CursoEmVideoAulas/src_exercicios/aula07_desafio12.py
@@ -0,0 +1,3 @@
+precoProduto = float(input('Digite o valor do produto:'))
+precoProdutoComDesconto = precoProduto*0.95
+print('Preco original {}, preco com desconto {:.2f}' .format(precoProduto,precoProdutoComDesconto))
