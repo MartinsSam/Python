bf9b7e0be5846d70cfb430ea31b06b352035262b
SFM <MartinsSam@users.noreply.github.com>
2019-07-09 21:56:32 -0300

    ajustes nos exercícios após acompanhamento do video de resolução

--- CursoEmVideoAulas/src_exercicios/aula07_desafio12.py
+++ CursoEmVideoAulas/src_exercicios/aula07_desafio12.py
@@ -1,3 +1,4 @@
 precoProduto = float(input('Digite o valor do produto:'))
+#100% - 5% = 95%(0.95)
 precoProdutoComDesconto = precoProduto*0.95
 print('Preco original {}, preco com desconto {:.2f}' .format(precoProduto,precoProdutoComDesconto))
--- CursoEmVideoAulas/src_exercicios/aula07_desafio13.py
+++ CursoEmVideoAulas/src_exercicios/aula07_desafio13.py
@@ -3,4 +3,4 @@ salario = float(input('Digite o salário do funcionário:'))
 aumento = 0.15
 #1.0 representa 100 do salario já existente
 novoSalario = salario * (1.0+aumento)
-print('O novo salario reajustado é de {}' .format(novoSalario))
+print('O novo salario reajustado é de {:.2f}' .format(novoSalario))
